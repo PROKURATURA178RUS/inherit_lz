f681d0ecdd965ed9b0ada4ae0d6f246d34f87d53
PROKURATURA178RUS <priora05tonirovka@tutamail.com>
2025-03-20 13:31:52 +0300

    camit

---
+++ Employee1.py
@@ -0,0 +1,43 @@
+class Employee:
+    def __init__(self,name,position,salary):
+        self.name = name
+        self.position = position
+        self.salary = salary
+    
+
+    def info():
+        return(print(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}'))
+
+
+    def calc_salary(self,worked_days):
+        month = input('Write month')
+        if month == 'January':
+            worked_days = 20
+        elif month =='February':
+            worked_days = 20
+        elif month == 'March':
+            worked_days = 21
+        elif month == 'April':
+            worked_days = 21
+        elif month == 'May':
+            worked_days = 20
+        elif month == 'June':
+            worked_days = 21
+        elif
+            
+        return(print(position*salary*worked_days))
+
+
+class Manager(Employee):
+    def __init__(self,department,num_employees):
+        super().__init__(name,position,salary)
+        self.department = department
+        self.num_employees = num_employees
+
+    
+    def info():
+        return(print(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}; Department:{self.department}; Num employees{self.num_employees} '))
+
+    def calc_salary():
+        return(print(position*salary*worked_days + position*salary*worked_days*(1/num_employees)))
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from Employee1 import Employee
+from Employee1 import Manager 
+
+month = input('Write month')
+name1 = input('Wtite name:')
+pos = float(input('Write position:'))
+sal = float(input('Write salary:'))
+Emp =Employee(name1,pos,sal)
+
+department = input('Write departmant:')
+num = int(input('Write number of employees:'))
