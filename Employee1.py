24ecac661eebab6ab08d9917ec6ed044699f18b3
PROKURATURA178RUS <priora05tonirovkas@tutamail.com>
2025-03-20 18:59:21 +0300

    comit2

--- Employee1.py
+++ Employee1.py
@@ -5,38 +5,25 @@ class Employee:
         self.salary = salary
     
 
-    def info():
-        return(print(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}'))
+    def info(self):
+        return(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}')
 
 
-    def calc_salary(self,worked_days):
-        month = input('Write month')
-        if month == 'January':
-            worked_days = 20
-        elif month =='February':
-            worked_days = 20
-        elif month == 'March':
-            worked_days = 21
-        elif month == 'April':
-            worked_days = 21
-        elif month == 'May':
-            worked_days = 20
-        elif month == 'June':
-            worked_days = 21
-            
-        return(print(position*salary*worked_days))
+    def calc_salary(self,worked_days):           
+        return(self.position*self.salary*worked_days)
 
 
 class Manager(Employee):
-    def __init__(self,department,num_employees):
+    def __init__(self,department,num_employees,name,position,salary,worked_days):
         super().__init__(name,position,salary)
         self.department = department
         self.num_employees = num_employees
+        self.worked_days = worked_days
 
     
-    def info():
-        return(print(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}; Department:{self.department}; Num employees{self.num_employees} '))
+    def info(self):
+        return(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}; Department:{self.department}; Num employees{self.num_employees} ')
 
-    def calc_salary():
-        return(print(position*salary*worked_days + position*salary*worked_days*(1/num_employees)))
+    def calc_salary(self):
+        return(self.position*self.salary*self.worked_days + self.position*self.salary*self.worked_days*(1/self.num_employees))
 
--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from Employee1 import Employee
 from Employee1 import Manager 
 
-month = input('Write month')
+
 name1 = input('Wtite name:')
 pos = float(input('Write position:'))
 sal = float(input('Write salary:'))
+days = int(input('Write working days amount:'))
 Emp =Employee(name1,pos,sal)
+
+print(f'Employee info:',Emp.info())
+print(f'Slary:',Emp.calc_salary(days))
 
 department = input('Write departmant:')
 num = int(input('Write number of employees:'))
+name2 = input('Write name:')
+pos1 = float(input('Write position:'))
+sal1 = float(input('Write salary:'))
+man = Manager(department,num,name2,pos1,sal1,days)
+
+print(f'Manager info:',man.info())
+print(f'Slary:',man.calc_salary())
