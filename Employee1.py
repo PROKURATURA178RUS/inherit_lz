7d5f6c5f0ec2697f8cbacf535ec2373c695a30ea
PROKURATURA178RUS <priora05tonirovkas@tutamail.com>
2025-03-20 21:31:55 +0300

    comit3

--- Employee1.py
+++ Employee1.py
@@ -1,29 +1,68 @@
+# Импортируем библиотеку, которая поможет нам посчитать кол-во рабочих дней в месяце
+import datetime 
+
+
+#Создаем родительский класс
 class Employee:
     def __init__(self,name,position,salary):
         self.name = name
         self.position = position
         self.salary = salary
     
-
+#Создаем метод, который покажет всю информацию про класс
     def info(self):
         return(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}')
+        
 
+#Создаем метод, который посчитает зарплату
+    def calc_salary(self,year,month):
+    # Определяем первый и последний день месяца
+            first_day = datetime.date(year, month, 1)
+            if month == 12:
+                last_day = datetime.date(year + 1, 1, 1) - datetime.timedelta(days=1)
+            else:
+                last_day = datetime.date(year, month + 1, 1) - datetime.timedelta(days=1)
+    
+    # Считаем количество рабочих дней
+            worked_days = 0
+            current_day = first_day
+            while current_day <= last_day:
+                if current_day.weekday() < 5:  # Понедельник (0) - Пятница (4)
+                    worked_days += 1
+                current_day += datetime.timedelta(days=1)
+       
+            return(self.position*self.salary*worked_days)
 
-    def calc_salary(self,worked_days):           
-        return(self.position*self.salary*worked_days)
-
-
+#Создаем дочерний класс
 class Manager(Employee):
-    def __init__(self,department,num_employees,name,position,salary,worked_days):
+    def __init__(self,department,num_employees,name,position,salary):
         super().__init__(name,position,salary)
         self.department = department
         self.num_employees = num_employees
-        self.worked_days = worked_days
+        
 
-    
+#Создаем метод, который покажет всю информацию про класс
     def info(self):
-        return(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}; Department:{self.department}; Num employees{self.num_employees} ')
+        return(f'Name:{self.name}; Position:{self.position}; Salary:{self.salary}; Department:{self.department}; Num employees:{self.num_employees} ')
 
-    def calc_salary(self):
-        return(self.position*self.salary*self.worked_days + self.position*self.salary*self.worked_days*(1/self.num_employees))
+
+#Создаем метод, который посчитает зарплату
+    def calc_salary(self,year,month):
+    # Определяем первый и последний день месяца
+            first_day = datetime.date(year, month, 1)
+            if month == 12:
+                last_day = datetime.date(year + 1, 1, 1) - datetime.timedelta(days=1)
+            else:
+                last_day = datetime.date(year, month + 1, 1) - datetime.timedelta(days=1)
+    
+    # Считаем количество рабочих дней
+            worked_days = 0
+            current_day = first_day
+            while current_day <= last_day:
+                if current_day.weekday() < 5:  # Понедельник (0) - Пятница (4)
+                    worked_days += 1
+                current_day += datetime.timedelta(days=1)
+       
+            
+            return(self.position*self.salary*worked_days + self.position*self.salary*worked_days*(1/self.num_employees))
 
--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+#Импортируем классы из файла
 from Employee1 import Employee
 from Employee1 import Manager 
 
-
+#Команды, дающие возможность пользователю самостоятельно ввести данные
 name1 = input('Wtite name:')
 pos = float(input('Write position:'))
 sal = float(input('Write salary:'))
-days = int(input('Write working days amount:'))
+date = int(input('Write year:'))
+date1 = int(input('Write month:'))
 Emp =Employee(name1,pos,sal)
 
+#Вывод значений
 print(f'Employee info:',Emp.info())
-print(f'Slary:',Emp.calc_salary(days))
+print(f'Slary:',Emp.calc_salary(date,date1))
 
+#Команды, дающие возможность пользователю самостоятельно ввести данные
 department = input('Write departmant:')
 num = int(input('Write number of employees:'))
 name2 = input('Write name:')
 pos1 = float(input('Write position:'))
 sal1 = float(input('Write salary:'))
-man = Manager(department,num,name2,pos1,sal1,days)
+man = Manager(department,num,name2,pos1,sal1)
 
+#Вывод значений
 print(f'Manager info:',man.info())
-print(f'Slary:',man.calc_salary())
+print(f'Slary:',man.calc_salary(date,date1))
